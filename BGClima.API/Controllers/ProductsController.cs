8ad74ea7832cf8d7fe5288a1352bf839309236d8
Polichronov <al.polichronov@gmail.com>
2025-07-09 18:09:51 +0300

    Remove unused controllers and views.

--- BGClima.API/Controllers/ProductsController.cs
+++ BGClima.API/Controllers/ProductsController.cs
@@ -1,7 +1,4 @@
-// Developers: Create the Views/Products folder and add Index.cshtml, Details.cshtml, Create.cshtml, Edit.cshtml, and Delete.cshtml views for this controller.
-// This controller has been converted from an API controller to an MVC controller with views.
 using BGClima.Application.Services;
-using BGClima.Domain.Entities;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BGClima.API.Controllers
@@ -15,88 +12,7 @@ namespace BGClima.API.Controllers
             _productService = productService;
         }
 
-        // GET: /Products
-        // View: Views/Products/Index.cshtml
-        public async Task<IActionResult> Index()
-        {
-            var products = await _productService.GetAllAsync();
-            return View(products);
-        }
 
-        // GET: /Products/Details/5
-        // View: Views/Products/Details.cshtml
-        public async Task<IActionResult> Details(int id)
-        {
-            var product = await _productService.GetProductWithDetailsAsync(id);
-            if (product == null) return NotFound();
-            return View(product);
-        }
-
-        // GET: /Products/Create
-        // View: Views/Products/Create.cshtml
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: /Products/Create
-        // View: Views/Products/Create.cshtml
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create(Product product)
-        {
-            if (ModelState.IsValid)
-            {
-                var created = await _productService.CreateAsync(product);
-                return RedirectToAction(nameof(Details), new { id = created.Id });
-            }
-            return View(product);
-        }
-
-        // GET: /Products/Edit/5
-        // View: Views/Products/Edit.cshtml
-        public async Task<IActionResult> Edit(int id)
-        {
-            var product = await _productService.GetProductWithDetailsAsync(id);
-            if (product == null) return NotFound();
-            return View(product);
-        }
-
-        // POST: /Products/Edit/5
-        // View: Views/Products/Edit.cshtml
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, Product product)
-        {
-            if (id != product.Id) return BadRequest();
-            if (ModelState.IsValid)
-            {
-                await _productService.UpdateAsync(product);
-                return RedirectToAction(nameof(Details), new { id = product.Id });
-            }
-            return View(product);
-        }
-
-        // GET: /Products/Delete/5
-        // View: Views/Products/Delete.cshtml
-        public async Task<IActionResult> Delete(int id)
-        {
-            var product = await _productService.GetProductWithDetailsAsync(id);
-            if (product == null) return NotFound();
-            return View(product);
-        }
-
-        // POST: /Products/Delete/5
-        // View: Views/Products/Delete.cshtml
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            await _productService.DeleteAsync(id);
-            return RedirectToAction(nameof(Index));
-        }
-
-        // API: GET /api/products
         [HttpGet("/api/products")]
         [Produces("application/json")]
         public async Task<IActionResult> GetAllProductsApi()
