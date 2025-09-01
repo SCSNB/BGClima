8ac8f7042164327257546fdc42c07131db936f8d
Polichronov <al.polichronov@gmail.com>
2025-09-01 19:35:43 +0300

    repair app.module.

--- BGClima.UI/src/app/app.module.ts
+++ BGClima.UI/src/app/app.module.ts
@@ -47,6 +47,26 @@ import { ProductCategoryComponent } from './components/product-category/product-
 import { ProductFiltersComponent } from './components/product-filters/product-filters.component';
 import { MaintenanceComponent } from './components/maintenance/maintenance.component';
 import { PromoProductsComponent } from './pages/promo-products/promo-products.component';
+import { SharedModule } from './shared/shared.module';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatExpansionModule } from '@angular/material/expansion';
 // FilterDialogComponent is declared and exported by SharedModule
 
 @NgModule({
@@ -111,7 +131,8 @@ import { PromoProductsComponent } from './pages/promo-products/promo-products.co
     MatSlideToggleModule,
     MatTooltipModule,
     MatDialogModule,
-    MatButtonModule,
+    MatButtonModule
+  ],
   providers: [
     AuthService,
     AuthGuard,
